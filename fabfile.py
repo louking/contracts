239d92692cfa8911a208bceeab23020ff3d3c4df
Lou King <lking@pobox.com>
2019-01-10 17:47:03 -0500

    create fab file for deployment
    * fixes #105

--- fabfile.py
+++ fabfile.py
@@ -7,42 +7,55 @@
 fabfile  -- deployment using Fabric
 =================================================================
 
+expecting fabric.json with following content
+    {
+        "connect_kwargs": {
+            "key_filename": sshkeyfilename (export OpenSSH key from puttygen)
+        },
+        "user": "contractsmgr"
+    }
+
+execute as follows
+
+    fab -H <target-host> deploy
+
+or 
+
+    fab -H <target1>,<target2> deploy
+
+if you need to check out a particular branch
+
+    fab -H <target-host> deploy --branchname=<branch>
+
 '''
 
-from fabric.api import env, run, cd
+from fabric import task
+from invoke import Exit
 
-USERNAME = 'contractsmgr'
 APP_NAME = 'contracts'
 WSGI_SCRIPT = 'contracts.wsgi'
 
-project_dir = ''
+@task
+def deploy(c, branchname='master'):
+    print 'c.user={} c.host={} branchname={}'.format(c.user, c.host, branchname)
 
-def sandbox():
-    server = 'sandbox.contracts.loutilities.com'
-    global project_dir
-    project_dir = '/var/www/{}/{}'.format(server, APP_NAME)
-    env.hosts = ["{}@{}".format(USERNAME, server)]
+    venv_dir = '/var/www/{server}/venv'.format(server=c.host)
+    project_dir = '/var/www/{server}/{appname}/{appname}'.format(server=c.host, appname=APP_NAME)
 
-def beta():
-    server = 'beta.contracts.loutilities.com'
-    global project_dir
-    project_dir = '/var/www/{}/{}'.format(server, APP_NAME)
-    env.hosts = ["{}@{}".format(USERNAME, server)]
+    c.run('cd {} && git pull'.format(project_dir))
+    
+    if not c.run('cd {} && git show-ref --verify --quiet refs/heads/{}'.format(project_dir, branchname), warn=True):
+        raise Exit('branchname {} does not exist'.format(branchname))
 
-def prod():
-    server = 'www.contracts.loutilities.com'
-    global project_dir
-    project_dir = '/var/www/{}/{}'.format(server, APP_NAME)
-    env.hosts = ["{}@{}".format(USERNAME, server)]
-
-def deploy(branchname='master'):
-    with cd(project_dir):
-        run('git pull')
-        run('git checkout {}'.format(branchname))
-        run('cp -R ../libs/js  contracts/static')
-        run('cp -R ../libs/css contracts/static')
-        # must source bin/activate before each command which must be done under venv
-        # because each is a separate process
-        run('source bin/activate; pip install -r requirements.txt')
-        run('source bin/activate; alembic -c contracts/alembic.ini upgrade head')
-        run('touch {}'.format(WSGI_SCRIPT))
+    c.run('cd {} && git checkout {}'.format(project_dir, branchname))
+    c.run('cd {} && cp -R ../../libs/js  contracts/static'.format(project_dir))
+    # must source bin/activate before each command which must be done under venv
+    # because each is a separate process
+    c.run('cd {} && source {}/bin/activate && pip install -r requirements.txt'.format(project_dir, venv_dir))
+    
+    versions_dir = '{}/contracts/versioning/versions'.format(project_dir)
+    if not c.run('test -d {}'.format(versions_dir), warn=True):
+        c.run('mkdir -p {}'.format(versions_dir))
+    
+    c.run('cd {} && source {}/bin/activate && alembic -c contracts/alembic.ini upgrade head'.format(project_dir, venv_dir))
+    c.run('cd {} && touch {}'.format(project_dir, WSGI_SCRIPT))
