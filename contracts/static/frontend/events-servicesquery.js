40a3bb2e53c959ca8372cba00aed916578af7356
Lou King <lking@pobox.com>
2020-01-18 13:02:30 -0500

    eventsservicesquery now validated correctly
    * fixes #284

--- contracts/static/frontend/events-servicesquery.js
+++ contracts/static/frontend/events-servicesquery.js
@@ -1,3 +1,5 @@
   $(document).ready(function(){
     $('select').formSelect();
+    // see https://stackoverflow.com/a/40124502/799921
+    $("select[required]").css({display: "block", height: 0, padding: 0, width: 0, position: 'absolute', left: '50%'});
   });
