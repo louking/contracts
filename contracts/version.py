21e5af8f8609244f5b10dbc8c0f7abb675b5ec7c
Lou King <lking@pobox.com>
2020-01-25 11:57:14 -0500

    pypi package updates: loutilities-3.0.1, mod-wsgi-4.7.0 (new)

--- contracts/version.py
+++ contracts/version.py
@@ -1,3 +1,2 @@
 # this string is used for the version string in the documentation, as well as the egg
-# TODO: check out msm-header.png on target before release
-__version__ = '2.0.0'
+__version__ = '2.0.1.dev1'
