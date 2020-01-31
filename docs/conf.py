2e70e86c8ea7f4b6bf42d2d4b132df756b5aac2f
Lou King <lking@pobox.com>
2020-01-31 14:44:22 -0800

    docs use Read the Docs Sphinx Theme

--- docs/conf.py
+++ docs/conf.py
@@ -80,14 +80,19 @@ pygments_style = None
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-#
-html_theme = 'alabaster'
+# see https://sphinx-rtd-theme.readthedocs.io/en/stable/index.html
+html_theme = 'sphinx_rtd_theme'
+html_theme_path = ['_themes', ]
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
-#
-# html_theme_options = {}
+# set navigation depth
+html_theme_options = {
+    'navigation_depth': -1,
+    'collapse_navigation': False,
+    'sticky_navigation': True,
+}
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
