93022467d40c7d5a21e292ee448feb045e06e65f
Lou King <lking@pobox.com>
2022-08-08 16:40:56 -0400

    version 2.1.5

--- contracts/version.py
+++ contracts/version.py
@@ -1,5 +1,5 @@
 # this string is used for the version string in the documentation, as well as the egg
-__version__ = '2.1.3'
+__version__ = '2.1.5'
 __docversion__ = __version__
 # uncomment for development
 # __docversion__ = 'latest'
