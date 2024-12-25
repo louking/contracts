ccfedbe72f44af44bc30c115186ccb337c7cb5ad
Lou King <lking@pobox.com>
2024-12-25 05:38:07 -0500

    version picks up from .env (#500)

--- app/src/contracts/version.py
+++ app/src/contracts/version.py
@@ -1,5 +1,6 @@
-# this string is used for the version string in the documentation, as well as the egg
-__version__ = '3.0.0.dev1'
+from os import environ
+
+__version__ = environ['APP_VER']    # from .env
 __docversion__ = __version__
 # uncomment for development
 # __docversion__ = 'latest'
